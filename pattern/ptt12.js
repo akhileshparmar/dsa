a73135a4f36c35d8ecc2bf358217753ef8534097
akhiparmar <akhileshparmar.d4@gmail.com>
2024-06-12 20:37:44 +0530

    created pattern 12

---
+++ pattern/ptt12.js
@@ -0,0 +1,18 @@
+let pattern = "";
+
+for (let i = 1; i <= 4; i++) {
+  for (let j = 1; j <= 4 - i; j++) {
+    pattern += " ";
+  }
+  for (let j = 1; j <= i; j++) {
+    pattern += "*";
+  }
+  pattern += "\n";
+}
+
+console.log(pattern);
+
+//    *
+//   **
+//  ***
+// ****
