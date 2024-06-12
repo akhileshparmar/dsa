6ee44f0749ec4b2a63837df52d136a70452a1052
akhiparmar <akhileshparmar.d4@gmail.com>
2024-06-12 20:20:14 +0530

    created pattern 4

---
+++ pattern/ptt4.js
@@ -0,0 +1,15 @@
+let pattern = "";
+
+for (let i = 1; i <= 4; i++) {
+  for (let j = 1; j <= i; j++) {
+    pattern += i;
+  }
+  pattern += "\n";
+}
+
+console.log(pattern);
+
+// 1
+// 22
+// 333
+// 4444
